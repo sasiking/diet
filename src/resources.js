5457e7f48821de2d3d4c6e1b98b6941828537d0c
Sasi Kiran <sasikiran276@gmail.com>
2021-11-19 17:12:54 +0530

    Add files via upload

---
+++ src/resources.js
@@ -0,0 +1,65 @@
+const category_dairy = [
+    {Name:"cheese", Quantity:1, cal:61, c:0, p:4, f:5},
+    {Name:"Paneer", Quantity:100, cal:310, c:1, p:18, f:26},
+    {Name:"ghee", Quantity:10, cal:90, c:0, p:0, f:10},
+    {Name:"curd", Quantity:100, cal:66, c:3, p:4, f:4},
+    {Name:"Cream-MF", Quantity:10, cal:38, c:0.3, p:0.2, f:4},
+    {Name:"WHEY", Quantity:1, cal:113, c:1, p:25, f:1}
+];
+const category_veg = [
+    {Name:"rajma", Quantity:100, cal:333, c:60, p:24, f:1},
+    {Name:"oats", Quantity:100, cal:402, c:69.4, p:11.8, f:8.6},
+    {Name:"S.CHUNKS", Quantity:100, cal:336, c:34, p:50, f:0},
+    {Name:"ChickPeas", Quantity:10, cal:36.4, c:6.1, p:1.9, f:0.6},
+    {Name:"Avocado", Quantity:200, cal:320, c:2, p:4, f:30},
+    {Name:"Chana Dal", Quantity:100, cal:347, c:52, p:22, f:2},
+    {Name:"Tur daal", Quantity:100, cal:343, c:63, p:22, f:1.5},
+    {Name:"Yellow moong", Quantity:100, cal:31, c:1, p:13, f:0},
+    {Name:"Black chana", Quantity:100, cal:53, c:0, p:20.5, f:6}
+];
+
+const category_non_veg = [
+    {Name:"eggs", Quantity:1, cal:71.4, c:60, p:6, f:5},
+    {Name:"egg whites", Quantity:1, cal:14.4, c:69.4, p:3.6, f:0},
+    {Name:"Salmon", Quantity:100, cal:170, c:34, p:26, f:6},
+    {Name:"chicken", Quantity:100, cal:239, c:6.1, p:27, f:9},
+    {Name:"Chicken T", Quantity:100, cal:177, c:2, p:24, f:8},
+    {Name:"Chicken B", Quantity:100, cal:165, c:52, p:31, f:3.6},
+    {Name:"Liver mutton", Quantity:100, cal:138, c:63, p:20.4, f:5},
+    {Name:"heart", Quantity:100, cal:120, c:1, p:16.5, f:5.7},
+    {Name:"Kidneys", Quantity:100, cal:95, c:0, p:15.7, f:2.9},
+    {Name:"Brain", Quantity:100, cal:120, c:1, p:10.4, f:8.6},
+    {Name:"paya", Quantity:1, cal:193, c:0, p:19, f:9.8}
+];
+
+const category_nuts = [
+    {Name:"Macademia", Quantity:10, cal:70,c:1.4,p:1,f:6.7},
+    {Name:"Brazil", Quantity:10, cal:68,c:0,p:1.4,f:7.2},   
+    {Name:"Pine", Quantity:10, cal:67.3,c:1.3,p:1.4,f:6.8}, 
+    {Name:"pecan", Quantity:10, cal:72,c:1,p:1,f:6.3},      
+    {Name:"hazlenuts", Quantity:10, cal:67,c:1,p:1.4,f:5},  
+    {Name:"almonds", Quantity:10, cal:61,c:2,p:2,f:6.3},    
+    {Name:"Walnuts", Quantity:10, cal:68,c:1.2,p:1.4,f:4.4},
+    {Name:"Pista", Quantity:10, cal:56.2,c:2.8,p:2,f:4.5},
+    {Name:"Cashew", Quantity:10, cal:55.3,c:3,p:1.8,f:5.3}
+];
+
+const category_seeds = [
+    {Name:"Sunflower", Quantity:10, cal:63,c:1,p:2,f:2.4},
+    {Name:"Pumpkin", Quantity:10, cal:35,c:1,p:2.5,f:4},      
+    {Name:"flax", Quantity:10, cal:45,c:0,p:2,f:3},
+    {Name:"chia", Quantity:10, cal:48,c:1,p:1.7,f:4.7},       
+    {Name:"watermelon", Quantity:10, cal:55,c:1.5,p:2.8,f:10},
+    {Name:"Sabja", Quantity:10, cal:52,c:3,p:11,f:0}
+];
+
+const category_millets = [
+    {Name:"Rice", Quantity:100, cal:360,c:78,p:6.5,f:0.5},
+    {Name:"Wheat", Quantity:100, cal:362,c:59,p:11.8,f:1.7},
+    {Name:"Ragi", Quantity:100, cal:328,c:61.5,p:7.3,f:1.3},
+    {Name:"Quinoa", Quantity:100, cal:368,c:57,p:14,f:6.1},
+    {Name:"oats", Quantity:100, cal:402,c:58.8,p:11.8,f:8.6},        
+    {Name:"Foxtail(Korra)", Quantity:100, cal:351,c:56.5,p:11.2,f:4},
+    {Name:"BrownTop(Andu)", Quantity:100, cal:340,c:62,p:8,f:2},      
+    {Name:"Kodo(Arikelu)", Quantity:100, cal:378,c:64,p:11,f:4.2}
+];
