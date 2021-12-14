43486e87641fa48aee66434f92536b5a7664ae0d
Sasi Kiran <sasikiran276@gmail.com>
2021-12-14 08:54:41 +0530

    Updated the scr files to remove bugs

--- src/bmr.js
+++ src/bmr.js
@@ -62,6 +62,45 @@ const updateResult = () =>{
 		totalcls.innerHTML = total;
 	}
 };
+var div_main = document.createElement("DIV");
+
+
+const btnRemove = ()=> {
+
+	const checkboxes = document.querySelectorAll('input[name="para-val"]:checked');
+	let colors = [];
+
+	checkboxes.forEach((checkbox) => {
+		colors.push(checkbox.value);
+	});
+
+	if(colors.length===0){
+		var div = document.getElementById("div-id-resource")
+		div.parentNode.removeChild(div);
+		div.innerHTML = "";
+		div_main.innerHTML = "";
+		var totalcls = document.getElementById("total-resources");
+		totalcls.innerHTML = "";
+	}
+	else{
+		checkboxes.forEach((checkbox)=>{
+			const value = checkbox.value;
+
+			var id = document.getElementById('p'+value);
+			id.parentNode.removeChild(id);
+
+			id = document.getElementById('chk' + value);
+			id.parentNode.removeChild(id);
+
+			var parr = resources.findIndex( (x) => x.name === value);
+			console.log(parr, value);
+			if( parr>-1){
+				resources.splice(parr, 1);
+			}
+			updateResult();
+		});
+	}
+};
 
 function AddDropDownList() {
 	//Build an array containing Customer records.
@@ -93,26 +132,34 @@ function AddDropDownList() {
 
 	//Reference the container DIV.
 	var dvContainer = document.getElementById("dvContainer");
-
+	// var selval = document.createElement("P");
+	// selval.innerHTML = sel;
+	// dvContainer.appendChild(selval);
 	var cat;
 	//console.log(sel)
 	if(sel==="Dairy"){
+		//console.log(category_dairy);
 		cat = category_dairy
 	}
 	else if(sel==="Nuts")
 	{
+		//console.log(category_nuts)
 		cat = category_nuts
 	}
 	else if(sel==="Veg"){
+		//console.log(category_veg)
 		cat = category_veg
 	}
 	else if(sel==="Millets"){
+		//console.log(category_millets)
 		cat = category_millets
 	}
 	else if(sel=="Seeds"){
+		//console.log(category_seeds)
 		cat = category_seeds
 	}
 	else{
+		//console.log(category_non_veg)
 		cat = category_non_veg
 	}
 	console.log(cat);
@@ -141,12 +188,15 @@ function AddDropDownList() {
 
 	var div = document.createElement("DIV");
 	div.className = "div-resource";
-	div.id = "div-id-resource"
-	div.appendChild(selcat);
+	div.id = "div-id-resource";
+
+
 
 
 
 	addBtn.onclick = function(){
+		var res_div = document.createElement("DIV");
+		res_div.className = "resource-sup-div";
 
 		var ind = cat.findIndex(x=>x.Name===selcat.value);
 		var radio = document.createElement("INPUT");
@@ -154,6 +204,7 @@ function AddDropDownList() {
 		radio.id = 'chk' + selcat.value;
 		radio.value = selcat.value;
 		radio.name = 'para-val';
+		radio.className = "resource-chk"
 
 		var resval = document.createElement("P");
 		resval.id = "p"+ selcat.value;
@@ -165,8 +216,7 @@ function AddDropDownList() {
 		var prot = cat[ind].p
 		var fats = cat[ind].f
 
-		var res ="Name = "+cat[ind].Name+ "  Quantity = "+qun+" Calories ="+ cals +" Carbs ="+ 
-		carbs +" Protien ="+ prot  +" Fats ="+ fats
+		var res =cat[ind].Name+ " "+qun+" "+ cals +" "+ carbs +" "+ prot  +" "+ fats
 		resval.innerHTML = res;
 		cnt++;
 		let resource = {};
@@ -179,53 +229,61 @@ function AddDropDownList() {
 		resources.push(resource);
 		updateResult();
 
-		div.appendChild(radio);
-		div.appendChild(resval);
+		res_div.appendChild(radio);
+		res_div.appendChild(resval);
+
+		div.appendChild(res_div);
 	}
 
-	//Create a Remove Button.
-	var btnRemove = document.createElement("INPUT");
-	btnRemove.value = "Remove";
-	btnRemove.type = "button";
-	btnRemove.className = "Button"
+	//Create a Remove Button. 
 
-	btnRemove.onclick = function () {
-
-		const checkboxes = document.querySelectorAll('input[name="para-val"]:checked');
-		let colors = [];
-
-		checkboxes.forEach((checkbox) => {
-    		colors.push(checkbox.value);
-		});
-
-		if(colors.length===0){
-			dvContainer.removeChild(this.parentNode);
-		}
-		else{
-			checkboxes.forEach((checkbox)=>{
-				const value = checkbox.value;
-
-				var id = document.getElementById('p'+value);
-				id.parentNode.removeChild(id);
-
-				id = document.getElementById('chk' + value);
-				id.parentNode.removeChild(id);
-
-				var parr = resources.findIndex( (x) => x.name === value);
-
-				if( parr>-1){
-					resources.splice(parr, 1);
-				}
-				updateResult();
-			});
-		}
-	};
-
-
-	//Add the Remove Buttton to DIV.
-	div.appendChild(addBtn);
-	div.appendChild(btnRemove);
+	var div_select = document.createElement("DIV");
+	div_select.className = "div-select-resource"
 	
+	//Add the Remove Buttton to DIV.
+	div_select.appendChild(selcat);
+	div_select.appendChild(addBtn);
+	dvContainer.appendChild(div_select);
+
+
+	var div_content = document.createElement("DIV");
+	div_content.className = "resource-div-content";
+
+	var para = document.createElement("P");
+	para.className = "resource-para-name";
+	para.innerHTML = "Name";
+	div_content.appendChild(para);
+
+	var paraq = document.createElement("P");
+	paraq.innerHTML = "Quantity";
+	paraq.className = "resource-para-name";
+	div_content.appendChild(paraq);
+
+	var parac = document.createElement("P");
+	parac.innerHTML = "Cals";
+	parac.className = "resource-para-name";
+	div_content.appendChild(parac);
+
+	var paracr = document.createElement("P");
+	paracr.innerHTML = "Carbs";
+	paracr.className = "resource-para-name";
+	div_content.appendChild(paracr);
+
+	var parap = document.createElement("P");
+	parap.className = "resource-para-name";
+	parap.innerHTML = "Protien";
+	div_content.appendChild(parap);
+
+	var paraf = document.createElement("P");
+	paraf.className = "resource-para-name";
+	paraf.innerHTML = "Fats";
+	div_content.appendChild(paraf);
+
 	//Add the DIV to the container DIV.
+	div_main.appendChild(div_select);
+	div_main.appendChild(div_content);
+
+	div.appendChild(div_main);
+
 	dvContainer.appendChild(div);
 }
